ea5e1e7faed478d7c1218ea1bfb0735372a79021
Dave <oconnorwork@gmail.com>
2015-01-11 15:43:58 -0500

    Added category/slug, category.html

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category, Page
 
 
 def index(request):
-    context_dict = {'boldmessage': 'I am bold font from the context'}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
     return render(request, 'rango/index.html', context_dict)
 
 
@@ -11,3 +12,19 @@ def about(request):
     context_dict = {'emphasized_message': 'This is italicized!'}
     return render(request, 'rango/about.html', context_dict)
 
+
+def category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = cat.name
+
+        context_dict['category'] = cat
+        pages = Page.objects.filter(category=cat)
+        context_dict['pages'] = pages
+
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html', context_dict)

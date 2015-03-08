5c0ec38b45008d8ce52a8656c005ab6cbd55eec2
Dave O'Connor <oconnorwork@gmail.com>
2015-03-08 13:07:56 -0400

    Implemented django-registration-redux

--- rango/views.py
+++ rango/views.py
@@ -114,69 +114,69 @@ def add_page(request, category_name_slug):
     return render(request, 'rango/add_page.html', context_dict)
 
 
-def register(request):
-    registered = False
-
-    if request.method == 'POST':
-        user_form = UserForm(data=request.POST)
-        profile_form = UserProfileForm(data=request.POST)
-
-        if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
-            user.save()
-
-            profile = profile_form.save(commit=False)
-            profile.user = user
-
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
-
-            profile.save()
-
-            registered = True
-
-            new_user = authenticate(username=request.POST['username'], password=request.POST['password'])
-            login(request, new_user)  # automatically logs in new user
-
-        else:
-            print user_form.errors, profile_form.errors
-
-    else:
-        user_form = UserForm()
-        profile_form = UserProfileForm()
-
-    return render(request, 'rango/register.html',
-                  {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
-
-
-def user_login(request):
-
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(username=username, password=password)
-
-        if user:
-            if user.is_active:
-                login(request, user)
-                return HttpResponseRedirect('/rango/')
-            else:
-                context = {'disabled': 'Your Rango account is currently disabled.'}
-                return render(request, 'rango/login.html', context)
-        else:
-            context = {'failure': 'Sorry, we do not recognize your username or password. Please try logging in again.'}
-            return render(request, 'rango/login.html', context)
-    else:
-        return render(request, 'rango/login.html', {})
+# def register(request):
+#     registered = False
+#
+#     if request.method == 'POST':
+#         user_form = UserForm(data=request.POST)
+#         profile_form = UserProfileForm(data=request.POST)
+#
+#         if user_form.is_valid() and profile_form.is_valid():
+#             user = user_form.save()
+#             user.set_password(user.password)
+#             user.save()
+#
+#             profile = profile_form.save(commit=False)
+#             profile.user = user
+#
+#             if 'picture' in request.FILES:
+#                 profile.picture = request.FILES['picture']
+#
+#             profile.save()
+#
+#             registered = True
+#
+#             new_user = authenticate(username=request.POST['username'], password=request.POST['password'])
+#             login(request, new_user)  # automatically logs in new user
+#
+#         else:
+#             print user_form.errors, profile_form.errors
+#
+#     else:
+#         user_form = UserForm()
+#         profile_form = UserProfileForm()
+#
+#     return render(request, 'rango/register.html',
+#                   {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
+#
+#
+# def user_login(request):
+#
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#
+#         user = authenticate(username=username, password=password)
+#
+#         if user:
+#             if user.is_active:
+#                 login(request, user)
+#                 return HttpResponseRedirect('/rango/')
+#             else:
+#                 context = {'disabled': 'Your Rango account is currently disabled.'}
+#                 return render(request, 'rango/login.html', context)
+#         else:
+#             context = {'failure': 'Sorry, we do not recognize your username or password. Please try logging in again.'}
+#             return render(request, 'rango/login.html', context)
+#     else:
+#         return render(request, 'rango/login.html', {})
 
 
 def restricted(request):
     context = {'success': 'You are authenticated!', 'failure': 'You are not logged in!'}
     return render(request, 'rango/restricted.html', context)
 
-@login_required()
-def user_logout(request):
-    logout(request)
-    return HttpResponseRedirect('/rango/')
+# @login_required()
+# def user_logout(request):
+#     logout(request)
+#     return HttpResponseRedirect('/rango/')
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -100,8 +101,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media direct
 MEDIA_URL = '/media/'
 
 # Login
-LOGIN_URL = '/rango/login/'
+# LOGIN_URL = '/rango/login/'
 
 # Sessions
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 # SESSION_COOKIE_AGE = 1209600
+
+# Registration and Login for django-registration-redux
+REGISTRATION_OPEN = True                # If True, users can register
+ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
+REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
+LOGIN_REDIRECT_URL = '/rango/'  # The page you want users to arrive at after they successful log in
+LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
+                                # and are trying to access pages requiring authentication

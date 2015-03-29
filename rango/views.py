85b71ad20b8063f73cad05af164b82a914ce20a4
DJO3 <dev@pytools.org>
2015-03-29 16:52:50 -0400

    minor tweaks to category view and sidebar name

--- rango/views.py
+++ rango/views.py
@@ -13,7 +13,7 @@ def encode_url(str):
 
 
 def index(request):
-    category_list = Category.objects.all()
+    category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
 
